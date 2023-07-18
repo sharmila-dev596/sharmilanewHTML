a65725a05bec6f08c944ee49ce4607cb13bac09c
sharmila-dev596 <sksharmila596@gmail.com>
2023-07-18 15:57:23 +0530

    first commit

---
+++ anchors.html1.bat
@@ -0,0 +1,15 @@
+<html>
+<head>
+<body>
+<br>
+<h1 edupoly</h1>
+<br>
+<a href="https://edupoly.in/">edupoly</a>
+<a href="https://edupoly.in/">solution hubstreet ,streetno3</a>
+<a href="https://edupoly.in/">patrika nagar hyderabad,telanagana 500081></a>
+<a href="https://edupoly.in/">instgram</a>
+<a href="https://edupoly.in/">facebook</a>
+<a href="https://edupoly.in/">demo class</a>
+</body>
+</head>
+</html>
